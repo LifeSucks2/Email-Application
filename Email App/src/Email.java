9af9eed0ac53238aad33ea2f6109767f0a4438e7
LifeSucks2 <56684387+LifeSucks2@users.noreply.github.com>
2023-02-25 15:28:53 +0100

    Email Klasse wurde angefangen

--- Email App/src/Email.java
+++ Email App/src/Email.java
@@ -1,3 +1,28 @@
 public class Email {
+    private String vorname, nachname, firma, abteilung;
+
+    public Email(String vornme, String nachnme, String frm, String abt){
+        this.vorname = vornme;
+        this.nachname = nachnme;
+        this.firma = frm;
+        this.abteilung = abt;
+        
+    }
+
+    public Email(String vornme, String nachnme, String frm){
+        this.vorname = vornme;
+        this.nachname = nachnme;
+        this.firma = frm;
+    }
+
+    public String emailToStringWithAbt(){
+        return this.vorname + "." + this.nachname + "@" + this.abteilung + "." + this.firma;
+    }
+
+    public String emailToStringWithoutAbt(){
+        return this.vorname + "." + this.nachname + "@" + this.firma;
+    }
     
+
+
 }
